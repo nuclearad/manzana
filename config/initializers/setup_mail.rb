949bc701c1f47cfdc8fd7c6406472d41a7c5e98e
Jonathan <jonathan.rojas@globalr.net>
2015-08-31 19:11:11 -0430

    configuracion final

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -12,11 +12,11 @@ ActionMailer::Base.smtp_settings = {
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
-  :domain               => "gmail.com",
+  :domain               => "localhost",
   :user_name            => "lamanzananaranja1@gmail.com",
   :password             => "lamanzana1naranja",
   :authentication       => "plain",
-  :enable_starttls_auto => false
+  :enable_starttls_auto => true
 }
 
 
