93b27de84b7528b1a1af018ab17065b36cc7054f
Jonathan <jonathan.rojas@globalr.net>
2015-08-31 17:58:28 -0430

    se agrega el token para autentificar el form

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,24 +1,21 @@
+=begin
 ActionMailer::Base.smtp_settings = {
   :address              => "smtp.gmail.com",
   :port                 => 587,
   :domain               => "gmail.com",
   :user_name            => "jrojas@nuclearagenciadigital.com",
-  :password             => "remolacho",
+  :password             => "",
   :authentication       => "plain",
   :enable_starttls_auto => true
 }
-
-=begin
-	
-  config.action_mailer.smtp_settings  = {
-      address: 'smtp.gmail.com',
-      port: 587,
-      domain: 'localhost',
-      authentication: 'plain',
-      enable_starttls_auto: true,
-      user_name: 'danielwebapps@gmail.com',
-      password: 'colombia01'
-  }
-
-	
 =end
+
+ActionMailer::Base.smtp_settings = {
+  :address              => "smtp.gmail.com",
+  :port                 => 587,
+  :domain               => "gmail.com",
+  :user_name            => "danielwebapps@gmail.com",
+  :password             => "colombia01",
+  :authentication       => "plain",
+  :enable_starttls_auto => true
+}
